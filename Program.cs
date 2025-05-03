7cf96fa433bd5570ca849a27e1051f8e51fcf1c8
yuikimura <edikanorusiruru@outlook.jp>
2025-05-04 02:56:18 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ちわ!");
+            int result = Add(3, 5);
+            Console.WriteLine("結果: " + result);
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
